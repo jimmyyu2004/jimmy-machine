e8c155938b33e7c324a66fb0e9bed6621d9e6a82
Jimmy Yu <jianmingyu2004@berkeley.edu>
2024-08-01 10:35:23 +0100

    Updates

---
+++ precache-manifest.e5a9c0b127ce755af0417264f943c8f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0733df23fcc2fb25f853dacbcbd33fc4",
+    "url": "/jimmy-machine/index.html"
+  },
+  {
+    "revision": "77311e8cb187bc2308ec",
+    "url": "/jimmy-machine/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "76d0ff16601a57c03aa8",
+    "url": "/jimmy-machine/static/js/2.3404488d.chunk.js"
+  },
+  {
+    "revision": "9adec3c570c04dd4ad55a2770a905a7a",
+    "url": "/jimmy-machine/static/js/2.3404488d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "594636e87ae14ce0653b",
+    "url": "/jimmy-machine/static/js/3.2dd38a63.chunk.js"
+  },
+  {
+    "revision": "77311e8cb187bc2308ec",
+    "url": "/jimmy-machine/static/js/main.fec4b7fb.chunk.js"
+  },
+  {
+    "revision": "56a6c1db146915846d0e",
+    "url": "/jimmy-machine/static/js/runtime-main.5269c24e.js"
+  }
+]);
